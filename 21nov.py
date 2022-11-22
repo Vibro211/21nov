9ccc2f3ae01940cd4ff433e02a8b4e415f5faccd
Vibro211 <Vibro211@users.noreply.github.com>
2022-11-22 13:00:14 +0300

    PHINALLY!!!!!!!1 (BUT SPECIAL THX TO github.com/SolidGray :D)

---
+++ 21nov.py
@@ -0,0 +1,18 @@
+from typing import List
+
+A = input("Введите массив строк через пробел: ").rstrip()
+A = A.split(" ")
+print(A)
+L = len(A)
+B = []
+
+for i in A:
+    if len(i) <= 3:
+        B.append(i)
+
+
+
+
+
+print("Элементы этого массива из 3 и менее символов: ")
+print(B)
